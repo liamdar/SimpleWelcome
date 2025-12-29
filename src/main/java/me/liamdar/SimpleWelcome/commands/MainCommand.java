1121fcea960142aaf499f11172b6ce352f9a9de4
Liam <unknownfor13d@gmail.com>
2025-12-29 21:34:56 +0000

    Added subcommand info

--- src/main/java/me/liamdar/SimpleWelcome/commands/MainCommand.java
+++ src/main/java/me/liamdar/SimpleWelcome/commands/MainCommand.java
@@ -7,6 +7,12 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+/*
+ In some weeks
+ ill try switching to
+ TriumpCMD, Deprecated v of the plugin.
+*/
+
 public class MainCommand implements CommandExecutor {
     private final JavaPlugin plugin;
 
@@ -15,7 +21,8 @@ public class MainCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label,
+            String[] args) {
         if (cmd.getName().equalsIgnoreCase("sw")) {
             if (args.length == 0 || (args.length == 1 && !args[0].equalsIgnoreCase("reload"))) {
                 sender.sendMessage("§eSimple§fWelcome §bv" + plugin.getDescription().getVersion());
@@ -35,8 +42,13 @@ public class MainCommand implements CommandExecutor {
                     sender.sendMessage(coloredMsg2);
                     return true;
                 }
+                if (args_length == 1 && args[0].equalsIgnoreCase("info")) {
+                    sender.sendMessage("§eSimple§fWelcome §bv" + plugin.getDescription().getVersion());
+                    sender.sendMessage("§7" + plugin.getDescription());
+                    return true;
+                }
+            }
         }
+        return true;
     }
-    return true;
-}
 }
